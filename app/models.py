e88ae179377f40924f8e95fb149050bcc0726795
Dean Vains <dean.vains@outlook.com>
2019-09-12 15:56:06 +0800

    Making the aapp runable

--- app/models.py
+++ app/models.py
@@ -3,4 +3,32 @@ from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
+"""
+class User():
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
 
+class expected():
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    month = db.Column(db.String(64))
+    year = db.Column(db.Integer)
+    value = db.Column(db.Integer)
+    fprofile = db.Column(db.Integer)
+    intclass = db.Column(db.String(64))
+    interest = db.Column(db.Integer) #not an integer
+    #donation = db.Column(db.Integer)
+    #spending = db.Column(db.Integer)
+    recap = db.Column(db.Integer)
+    distribution = db.Column(db.Integer) #not an integer
+    timeframe = db.Column(db.Integer)
+    #addContribution = db.Column(db.Integer)
+
+
+class actual():
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    body = db.Column(db.String(140))
+"""
--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,14 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db
 from werkzeug.urls import url_parse
-from calculations.ClassHS import classH, classS
+from calculations.ClassH import classH
 from calculations.ClassN import classN
 from calculations.ClassQ import classQ
-from calculations.ClassEFG import classEFG
+from calculations.ClassE import classE
 from calculations.ClassA import classA
+from calculations.ClassF import classF
+#from calculations.ClassG import classG
+#from calculations.ClassS import classS
 
 @app.route('/')
 @app.route('/homepage')
@@ -31,11 +34,11 @@ def calcs():
         addContribution = request.calculations['additionalContribution']
 
         if(intclass == "E"):
-            calc = classEFG(month,year,value,fprofile,intclass,interest,donation,recap,distribution,timeframe)
+            calc = classE(month,year,value,fprofile,intclass,interest,donation,recap,distribution,timeframe)
         elif(intclass == "F"):
-            calc = classEFG(month,year,value,fprofile,intclass,interest,donation,recap,distribution,timeframe)
-        elif(intclass == "G"):
-            calc = classEFG(month,year,value,fprofile,intclass,interest,donation,recap,distribution,timeframe)
+            calc = classF(month,year,value,fprofile,intclass,interest,donation,recap,distribution,timeframe)
+       # elif(intclass == "G"):
+       #     calc = classG(month,year,value,fprofile,intclass,interest,donation,recap,distribution,timeframe)
         elif(intclass == "H"):
             calc = classH(month,year,value,fprofile,intclass,interest,spending,addContribution,timeframe)
         elif(intclass == "A"):
@@ -44,8 +47,8 @@ def calcs():
             calc = classN(month,year,value,fprofile,intclass,interest,spending,addContribution,timeframe)
         elif(intclass == "Q"):
             calc = classQ(month,year,value,fprofile,intclass,interest,spending,addContribution,timeframe)
-        elif(intclass == "S"):
-            calc = classS(month,year,value,fprofile,intclass,interest,spending,addContribution,timeframe)
+        #elif(intclass == "S"):
+        #    calc = classS(month,year,value,fprofile,intclass,interest,spending,addContribution,timeframe)
 
         return render_template("calcs.html", title='Calculation Page', calc=calc)
     return render_template("calcs.html", title='Calculation Page')
