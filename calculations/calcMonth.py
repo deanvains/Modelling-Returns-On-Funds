e4fd81f0ebb794c976b1f3fcb3df7754cb1314a0
Champster95 <tanchiangpin@gmail.com>
2019-09-22 04:21:54 +0800

    Add files via upload

---
+++ calculations/calcMonth.py
@@ -0,0 +1,5 @@
+def calcMonths(startYear,year2,startMonth,month2):
+    if startMonth <= month2:
+        return int((year2 - startYear) * 12 + abs((month2-startMonth)))
+    else:  
+        return int((year2 - startYear - 1) * 12 + abs((month2-startMonth)))
