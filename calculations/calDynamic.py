e814403b48a120c7acb9915c1acdc24322f3ea30
Champster95 <tanchiangpin@gmail.com>
2019-09-23 16:46:34 +0800

    Add files via upload
    Added calDynamic to calculate dynamic values with just one string

---
+++ calculations/calDynamic.py
@@ -0,0 +1,33 @@
+from calculations.calcMonth import calcMonths
+
+
+def calcDyn(dynamicVal,startMonth,startYear,dur):
+    monthDict = {"jan" : 1,
+                 "feb" : 2,
+                 "mar" : 3,
+                 "apr" : 4,
+                 "may" : 5,
+                 "jun" : 6,
+                 "jul" : 7,
+                 "aug" : 8,
+                 "sep" : 9,
+                 "oct" : 10,
+                 "nov" : 11,
+                 "dec" : 12}
+
+    calc = {}
+    for i in range(dur*12+1):
+        calc[i] = 0
+    
+    dynVal = dynamicVal.split(" ")
+    for group in dynVal:
+        grpLst = group.split("-")
+        monthVal = grpLst[0].lower()
+        yearVal = grpLst[1]
+        moneyVal = int(grpLst[2])
+        startVal = calcMonths(startYear,int(yearVal),monthDict[startMonth.lower()],monthDict[monthVal])
+        for i in range(startVal,dur*12+1):
+            calc[i] = moneyVal
+            
+    return calc  
+    
