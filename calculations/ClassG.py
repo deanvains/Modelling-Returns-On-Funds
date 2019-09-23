87787219b7cb17695bcf94453c8e476069ab1906
Champster95 <tanchiangpin@gmail.com>
2019-09-23 16:48:22 +0800

    Add files via upload

--- calculations/ClassG.py
+++ calculations/ClassG.py
@@ -1,20 +1,21 @@
 def classG(month,year,value,fprofile,intclass,interest,donation,recap,distribution,timeframe):
     
-    monthDict = {"January" : 1,
-                 "February" : 2,
-                 "March" : 3,
-                 "April" : 4,
-                 "May" : 5,
-                 "June" : 6,
-                 "July" : 7,
-                 "August" : 8,
-                 "September" : 9,
-                 "October" : 10,
-                 "November" : 11,
-                 "December" : 12}
+    monthDict = {"jan" : 1,
+                 "feb" : 2,
+                 "mar" : 3,
+                 "apr" : 4,
+                 "may" : 5,
+                 "jun" : 6,
+                 "jul" : 7,
+                 "aug" : 8,
+                 "sep" : 9,
+                 "oct" : 10,
+                 "nov" : 11,
+                 "dec" : 12}
+
     
     
-    dateMonth = month
+    dateMonth = month.lower()
     dateYear = year
     fundValue = value
     fundProfile = fprofile
--- calculations/ClassQ.py
+++ calculations/ClassQ.py
@@ -1,19 +1,20 @@
 def classQ(month,year,value,fprofile,intclass,interest,spending,addContribution,timeframe):
-    monthDict = {"January" : 1,
-                 "February" : 2,
-                 "March" : 3,
-                 "April" : 4,
-                 "May" : 5,
-                 "June" : 6,
-                 "July" : 7,
-                 "August" : 8,
-                 "September" : 9,
-                 "October" : 10,
-                 "November" : 11,
-                 "December" : 12}
+    monthDict = {"jan" : 1,
+                 "feb" : 2,
+                 "mar" : 3,
+                 "apr" : 4,
+                 "may" : 5,
+                 "jun" : 6,
+                 "jul" : 7,
+                 "aug" : 8,
+                 "sep" : 9,
+                 "oct" : 10,
+                 "nov" : 11,
+                 "dec" : 12}
+
     
     
-    dateMonth = month
+    dateMonth = month.lower()
     dateYear = year
     timeFrame = timeframe
     fundValue = value
