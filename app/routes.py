c8ffa0f4455faf665354cf1d0fc0a384335e5aff
Champster95 <tanchiangpin@gmail.com>
2019-10-03 04:18:57 +0800

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -32,7 +32,7 @@ def calcs():
             form = calculationForm()
             if form.validate_on_submit():
                 try:
-                   month = form.month.data.strip()
+                    month = form.month.data.strip()
                     year = form.year.data
                     if len(str(year)) != 4:
                         raise Exception("Invalid Years")
